627e2cea350a8ee3a6e0632dc430c5264c2c4610
JEONG Seung Min <147719121+ut-jsmsscnl@users.noreply.github.com>
2023-11-26 12:22:56 +0000

    摩擦を制限し、接線方向の相対速度の向きが逆になるのを防止

--- fruit.h
+++ fruit.h
@@ -24,16 +24,18 @@ Fruit *createFruit(double x, double y, double r, char c) {
 }
 
 Vector getImpulse(Vector n, Vector v, double rm, double e, double mu) {
+    if(vecDot(n, v) > 0.) return (Vector){0., 0.};
     Vector vn = vecMult(n, vecDot(n, v));
-    Vector t = vecNormalize(vecSub(v, vn));
+    Vector vt = vecSub(v, vn);
+    Vector t = vecNormalize(vt);
     double j = rm * (1+e) * vecNorm(vn);
+    if(j * mu > rm * vecNorm(vt)) mu = rm * vecNorm(vt) / j;
     Vector jn = vecMult(n, j);
     Vector jt = vecMult(t, -j * mu);
     return vecAdd(jn, jt);
 }
 
 void boundCollision(Fruit *f, Vector n, double e, double mu) {
-    if(vecDot(n, f->v) > 0.) return;
     Vector j = getImpulse(n, f->v, f->m, e, mu);
     vecAddA(&(f->j), j);
 }
@@ -41,8 +43,8 @@ void boundCollision(Fruit *f, Vector n, double e, double mu) {
 void fruitCollision(Fruit *f1, Fruit *f2, double e, double mu) {
     Vector n = vecNormalize(vecSub(f1->x, f2->x));
     Vector v = vecSub(f1->v, f2->v);
-    if(vecDot(n, v) > 0.) return;
-    Vector j = getImpulse(n, v, 1 / (1 / f1->m + 1 / f2->m), e, mu);
+    double rm = 1 / (1 / f1->m + 1 / f2->m);
+    Vector j = getImpulse(n, v, rm, e, mu);
     vecAddA(&(f1->j), j);
     vecAddA(&(f2->j), vecMinus(j));
 }
